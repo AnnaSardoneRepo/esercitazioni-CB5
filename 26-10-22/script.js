08d5ea131bef39cc97d9d38661ee5581169ca7cf
Anna Sardone <aniceedmaster@gmail.com>
2022-10-26 16:30:22 +0200

    esercizio 1

---
+++ 26-10-22/script.js
@@ -0,0 +1,15 @@
+//prima prova esercizio
+
+let myIdentity = ["Mario", "Rossi", 30, "laurea" ];
+console.log(myIdentity);
+let indice = myIdentity.indexOf(30);
+myIdentity.splice(indice,1);
+
+console.log(myIdentity);
+let date = "ott 16 2000";
+myIdentity.push(date);
+console.log(myIdentity);
+let newIdentity = myIdentity.slice(0,2);
+console.log(newIdentity);
+let myString = newIdentity.join(", ");
+console.log(myString.toUpperCase());
