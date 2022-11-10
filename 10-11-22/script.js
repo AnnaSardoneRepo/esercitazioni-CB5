46ea7a3d484723d61a10f07dc0186d534c0ed13e
Anna Sardone <aniceedmaster@gmail.com>
2022-11-10 20:57:21 +0100

    esercizio 10-11-22 agggiunta loader

--- 10-11-22/script.js
+++ 10-11-22/script.js
@@ -3,24 +3,48 @@ const adviceId = document.getElementById("advice-id");
 const advice = document.getElementById("adviceText");
 const btn = document.getElementById("btn-generator");
 
-
 const urlAdvice = "https://api.adviceslip.com/advice";
+
+
+// seleziono loading div
+const loader = document.querySelector("#loading");
+
+// mostro loading
+function displayLoading() {
+  loader.classList.add("display");
+
+  // stop loading dopo tot secondi
+  setTimeout(() => {
+      loader.classList.remove("display");
+  }, 8000);
+}
+
+// nascondo loading 
+function hideLoading() {
+  loader.classList.remove("display");
+}
+
+
 /**
  * Get data from the end point
  *
  * @param {string} url
  */
 const getData = (URL) => {
+  displayLoading();
   fetch(URL, {
       cache: "no-cache"
     })
     .then((res) => res.json())
-    .then((res) => (adviceId.innerHTML = res.slip.id) + (advice.innerHTML = res.slip.advice))
-}
+    .then((data) => (adviceId.innerHTML = data.slip.id) + (advice.innerHTML = data.slip.advice))
+    .catch((error) => console.log(error))
+    .finally(() => loader.classList.remove("display"))
+  
+};
 
 console.log(urlAdvice);
 window.onload = getData(urlAdvice);
 
 btn.addEventListener("click", () => {
   getData(urlAdvice)
-})
+});
